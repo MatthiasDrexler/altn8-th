2c33d08d50f93f424dddb4da29a6766a461b15de
Matthias Drexler <matthias.drexler@andrena.de>
2023-12-02 18:32:25 +0100

    Register multiple configurables

---
+++ src/main/kotlin/de/andrena/tools/altn8th/settings/FileExtensionConfigurable.kt
@@ -0,0 +1,29 @@
+package de.andrena.tools.altn8th.settings
+
+import com.intellij.openapi.options.Configurable
+import de.andrena.tools.altn8th.settings.ui.SettingsUi
+import javax.swing.JComponent
+
+class FileExtensionConfigurable : Configurable {
+    private lateinit var settingsComponent: SettingsUi
+
+    override fun getDisplayName(): String {
+        return "AltN8-TH"
+    }
+
+    override fun createComponent(): JComponent {
+        val settingsState = SettingsPersistentStateComponent.getInstance().state
+        settingsComponent = SettingsUi(settingsState)
+        return settingsComponent.panel
+    }
+
+    override fun isModified(): Boolean {
+        val settingsState = SettingsPersistentStateComponent.getInstance().state
+        return settingsState.isEnabled != settingsComponent.enabledStatus
+    }
+
+    override fun apply() {
+        val settingsState = SettingsPersistentStateComponent.getInstance().state
+        settingsState.isEnabled = settingsComponent.enabledStatus
+    }
+}
