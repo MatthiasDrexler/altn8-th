aa62b3de9705b298216177838fb2f2e164d3c685
Matthias Drexler <matthias.drexler@andrena.de>
2023-12-03 23:32:26 +0100

    Update plugins to latest versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("java")
-    id("org.jetbrains.intellij") version "1.13.3"
-    kotlin("jvm") version "1.8.21"
+    id("org.jetbrains.intellij") version "1.16.1"
+    kotlin("jvm") version "1.9.21"
 }
 
 group = "de.andrena.tools"
