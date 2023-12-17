caac8e07ff64a50bb925a52adb1c1095a6d2eca5
Matthias Drexler <matthias.drexler@andrena.de>
2023-12-17 20:07:16 +0100

    Use jbr from nix store

--- shell.nix
+++ shell.nix
@@ -1,8 +1,16 @@
 { pkgs ? import <nixpkgs> {} }:
 
+let
+    pathToDownloadedJRE = "~/.gradle/caches/modules-2/files-2.1/com.jetbrains/jbre/jbr_jcef-17.0.6-linux-x64-b469.82/extracted/jbr_jcef-17.0.6-x64-b469";
+in
 pkgs.mkShell {
   buildInputs = (with pkgs; [
     jdk
     gradle
   ]);
+
+  shellHook =
+  ''
+    ln -sf ${pkgs.jdk.outPath} ${pathToDownloadedJRE}
+  '';
 }
