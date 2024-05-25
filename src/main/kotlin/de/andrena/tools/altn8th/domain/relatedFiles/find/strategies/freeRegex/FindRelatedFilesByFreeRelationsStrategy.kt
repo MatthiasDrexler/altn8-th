5b95cf0791496a8b12f204c05de372df94588fed
Matthias Drexler <matthias.drexler@andrena.de>
2024-05-25 11:11:32 +0200

    Add test scenario files for free relation strategy

--- src/main/kotlin/de/andrena/tools/altn8th/domain/relatedFiles/find/strategies/freeRegex/FindRelatedFilesByFreeRelationsStrategy.kt
+++ src/main/kotlin/de/andrena/tools/altn8th/domain/relatedFiles/find/strategies/freeRegex/FindRelatedFilesByFreeRelationsStrategy.kt
@@ -5,17 +5,21 @@ import de.andrena.tools.altn8th.domain.relatedFiles.Relation
 import de.andrena.tools.altn8th.domain.relatedFiles.RelationsByStrategy
 import de.andrena.tools.altn8th.domain.relatedFiles.find.strategies.FindRelatedFilesStrategy
 import de.andrena.tools.altn8th.domain.settings.SettingsState
+import de.andrena.tools.altn8th.domain.settings.types.FreeRelationSetting
 
 internal class FindRelatedFilesByFreeRelationsStrategy : FindRelatedFilesStrategy {
     override fun find(origin: File, allFiles: Collection<File>, settings: SettingsState): RelationsByStrategy {
         val freeRelationsMatchingOrigin = settings.freeRelations
             .filter { origin.nameWithFileExtension().matches(Regex(it.origin)) }
-        val relatedFiles = freeRelationsMatchingOrigin
-            .flatMap { freeRelation ->
-                allFiles
-                    .filter { it.nameWithFileExtension().matches(Regex(freeRelation.related)) }
-                    .map { Relation(origin, it, FreeRelationRelationType(freeRelation)) }
-            }
+        val relatedFiles = freeRelationsMatchingOrigin.flatMap {
+            findRelationsMatching(it, allFiles, origin)
+        }
         return RelationsByStrategy(this, relatedFiles)
     }
+
+    private fun findRelationsMatching(
+        freeRelation: FreeRelationSetting, allFiles: Collection<File>, origin: File
+    ): List<Relation> = allFiles
+        .filter { it.nameWithFileExtension().matches(Regex(freeRelation.related)) }
+        .map { Relation(origin, it, FreeRelationRelationType(freeRelation)) }
 }
