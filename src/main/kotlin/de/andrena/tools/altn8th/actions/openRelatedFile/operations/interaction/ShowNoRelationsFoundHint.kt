c24230a9b3a39465673381b90b3600c61462d0a2
Matthias Drexler <matthias.drexler@andrena.de>
2024-05-30 08:30:26 +0200

    Rename information hint

--- src/main/kotlin/de/andrena/tools/altn8th/actions/openRelatedFile/operations/interaction/ShowNoRelationsFoundHint.kt
+++ src/main/kotlin/de/andrena/tools/altn8th/actions/openRelatedFile/operations/interaction/ShowNoRelationsFoundHint.kt
@@ -1,8 +1,8 @@
 package de.andrena.tools.altn8th.actions.openRelatedFile.operations.interaction
 
 import com.intellij.openapi.actionSystem.AnActionEvent
-import de.andrena.tools.altn8th.adapter.interaction.InlineHint
+import de.andrena.tools.altn8th.adapter.interaction.InformationHint
 
 internal class ShowNoRelationsFoundHint(private val actionEvent: AnActionEvent) {
-    fun show() = InlineHint().showFor(actionEvent, "No corresponding file(s) found")
+    fun show() = InformationHint().showFor(actionEvent, "No corresponding file(s) found")
 }
