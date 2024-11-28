214652c73eb529cdc6308cb7f4897d87f9e22d81
Matthias Drexler <matthias.drexler@andrena.de>
2024-11-28 12:15:18 +0100

    Use a column layout for the general settings

--- src/main/kotlin/de/andrena/tools/altn8th/settings/ui/components/general/GeneralSettingsUi.kt
+++ src/main/kotlin/de/andrena/tools/altn8th/settings/ui/components/general/GeneralSettingsUi.kt
@@ -3,11 +3,11 @@ package de.andrena.tools.altn8th.settings.ui.components.general
 import com.intellij.ui.IdeBorderFactory
 import com.intellij.ui.components.JBCheckBox
 import com.intellij.ui.components.JBTextArea
+import com.intellij.ui.components.panels.VerticalLayout
 import de.andrena.tools.altn8th.domain.settings.SettingsState
 import de.andrena.tools.altn8th.internationalization.I18n
 import de.andrena.tools.altn8th.settings.ui.Ui
 import de.andrena.tools.altn8th.settings.ui.multilineLabel
-import java.awt.BorderLayout
 import javax.swing.JPanel
 
 private val TITLE = I18n.lazyMessage("altn8.settings.general")
@@ -20,9 +20,9 @@ internal class GeneralSettingsUi(private val settingsState: SettingsState) : Ui 
     private val caseInsensitiveMatchingCheckBox = JBCheckBox(CASE_INSENSITIVE_MATCHING_LABEL.get(), settingsState.caseInsensitiveMatching)
 
     override val panel: JPanel =
-        JPanel(BorderLayout(SPACING, SPACING)).apply {
-            this.add(JBTextArea(DESCRIPTION.get()).multilineLabel(), BorderLayout.NORTH)
-            this.add(caseInsensitiveMatchingCheckBox)
+        JPanel(VerticalLayout(SPACING)).apply {
+            add(JBTextArea(DESCRIPTION.get()).multilineLabel())
+            add(caseInsensitiveMatchingCheckBox)
         }
 
     init {
