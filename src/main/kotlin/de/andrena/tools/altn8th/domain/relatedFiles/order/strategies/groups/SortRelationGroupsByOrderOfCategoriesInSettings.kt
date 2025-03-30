8e75173b303bc25f3b4ea33e1e6bdc3b2e6ff653
Matthias Drexler <matthias.drexler@andrena.de>
2025-03-30 10:56:32 +0200

    Order by categories in postfix settings

--- src/main/kotlin/de/andrena/tools/altn8th/domain/relatedFiles/order/strategies/groups/SortRelationGroupsByOrderOfCategoriesInSettings.kt
+++ src/main/kotlin/de/andrena/tools/altn8th/domain/relatedFiles/order/strategies/groups/SortRelationGroupsByOrderOfCategoriesInSettings.kt
@@ -3,9 +3,29 @@ package de.andrena.tools.altn8th.domain.relatedFiles.order.strategies.groups
 import de.andrena.tools.altn8th.domain.relatedFiles.RelationGroup
 import de.andrena.tools.altn8th.domain.settings.SettingsState
 
-
-internal class SortRelationGroupsByOrderOfCategoriesInSettings(state: SettingsState) : SortRelationGroupsStrategy {
+internal class SortRelationGroupsByOrderOfCategoriesInSettings(
+    private val state: SettingsState
+) : SortRelationGroupsStrategy {
     override fun sort(relationGroups: Collection<RelationGroup>): List<RelationGroup> {
-        return relationGroups.toList()
+        val typeOccurrences = relationGroups
+            .flatMap { group -> group.relations }
+            .groupBy { it.type.name() }
+            .mapValues { it.value.size }
+        val mostFrequentTypeName = typeOccurrences.maxByOrNull { it.value }?.key
+
+        val configuredOrderBySetting = when {
+            mostFrequentTypeName?.contains("Prefix", ignoreCase = true) == true ->
+                state.prefixes.map { it.category }.distinct()
+
+            mostFrequentTypeName?.contains("FreeRegex", ignoreCase = true) == true ->
+                state.freeRegexes.map { it.category }.distinct()
+
+            else -> state.postfixes.map { it.category }.distinct()
+        }
+
+        return relationGroups.sortedWith(compareBy { relationGroup ->
+            val index = configuredOrderBySetting.indexOf(relationGroup.category)
+            if (index != -1) index else configuredOrderBySetting.size
+        })
     }
 }
