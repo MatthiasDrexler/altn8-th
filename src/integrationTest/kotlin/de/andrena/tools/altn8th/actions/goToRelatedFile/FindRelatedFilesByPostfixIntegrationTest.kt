1a766be9aedd6dc67b54a788420adb43da2635cd
Matthias Drexler <matthias.drexler@andrena.de>
2025-12-24 13:20:20 +0100

    Simplify integration test

--- src/integrationTest/kotlin/de/andrena/tools/altn8th/actions/goToRelatedFile/FindRelatedFilesByPostfixIntegrationTest.kt
+++ src/integrationTest/kotlin/de/andrena/tools/altn8th/actions/goToRelatedFile/FindRelatedFilesByPostfixIntegrationTest.kt
@@ -1,9 +1,8 @@
 package de.andrena.tools.altn8th.actions.goToRelatedFile
 
-import com.intellij.driver.sdk.invokeAction
+import com.intellij.driver.sdk.openFile
 import com.intellij.driver.sdk.ui.components.editorTabs
 import com.intellij.driver.sdk.ui.components.ideFrame
-import com.intellij.driver.sdk.ui.components.searchEverywherePopup
 import com.intellij.ide.starter.project.LocalProjectInfo
 import de.andrena.tools.altn8th.startIDEFor
 import org.junit.jupiter.api.Test
@@ -22,13 +21,7 @@ class FindRelatedFilesByPostfixIntegrationTest {
             LocalProjectInfo(Path("./src/integrationTest/resources/projects/postfix/single"))
         ) {
             ideFrame {
-                invokeAction("SearchEverywhere")
-                searchEverywherePopup {
-                    keyboard {
-                        enterText("DefaultEntity")
-                        enter()
-                    }
-                }
+                openFile("src/DefaultEntity.kt")
 
                 keyboard {
                     hotKey(KeyEvent.VK_ALT, KeyEvent.VK_8)
